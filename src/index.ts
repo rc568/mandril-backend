c8c5b5da5731f5ce0114940bcdddca6a8f697e75
irojas <isaac568r@gmail.com>
2025-07-14 10:18:12 -0500

    Revert "typeorm init"
    This reverts commit a03c3dbbdb39d0f7489e1ba04a9ffafd9e090088.

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,12 @@
-import 'reflect-metadata';
 import { routerApp } from './api/router';
 import { Server, type ServerOptions } from './api/server';
 import envs from './config/envs';
-import { AppDataSource } from './db/typeorm/db';
 
 (async () => {
   main();
 })();
 
 async function main() {
-  AppDataSource.initialize()
-    .then(() => {
-      console.log('Database connection established successfully');
-    })
-    .catch((error) => {
-      console.error('Error during Data Source initialization:', error);
-    });
-
   const options: ServerOptions = {
     port: envs.PORT,
     router: routerApp(),
