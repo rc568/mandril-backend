49c3c205eb609a9e09278d7557d4262175a884e8
irojas <isaac568r@gmail.com>
2025-09-05 12:03:45 -0500

    add drizzle-schema to db connection

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,5 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import envs from '../config/envs';
+import * as schema from '../db/schemas';
 
-export const db = drizzle({ connection: envs.DATABASE_URL, casing: 'snake_case' });
+export const db = drizzle({ connection: envs.DATABASE_URL, casing: 'snake_case', schema });
