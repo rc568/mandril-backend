4beb2750bbc8d59d2d075f566f3519334886f451
irojas <isaac568r@gmail.com>
2025-09-23 17:20:51 -0500

    refactor database connection setup and types for improved clarity

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,19 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
+import type { ExtractTablesWithRelations } from 'drizzle-orm';
+import { drizzle, type NodePgQueryResultHKT } from 'drizzle-orm/node-postgres';
+import type { PgTransaction } from 'drizzle-orm/pg-core';
+import { Pool } from 'pg';
 import envs from '../config/envs';
 import * as schema from '../db/schemas';
 
-export const db = drizzle({ connection: envs.DATABASE_URL, casing: 'snake_case', schema });
+const pool = new Pool({
+  connectionString: envs.DATABASE_URL,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+  maxUses: 7500,
+});
+
+export const db = drizzle(pool, { casing: 'snake_case', schema });
 
 export const successConnectionDb = async () => {
   try {
@@ -13,3 +24,6 @@ export const successConnectionDb = async () => {
     process.exit(1);
   }
 };
+
+export type Transaction = PgTransaction<NodePgQueryResultHKT, typeof schema, ExtractTablesWithRelations<typeof schema>>;
+export type Executor = typeof db | Transaction;
