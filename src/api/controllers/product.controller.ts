3cea560d412ee52e1208322f9046452cc376d5c2
irojas <isaac568r@gmail.com>
2025-09-29 21:41:51 -0500

    productcontroller: remove unnecessary error handling and improve response consistency

--- src/api/controllers/product.controller.ts
+++ src/api/controllers/product.controller.ts
@@ -1,5 +1,4 @@
 import type { Request, Response } from 'express';
-import { CustomError } from '../../domain/errors/custom.error';
 import type { ProductService } from '../services';
 
 export class ProductController {
@@ -18,14 +17,12 @@ export class ProductController {
 
   createProduct = async (req: Request, res: Response) => {
     const product = await this.productService.create(req.body);
-    if (!product) throw CustomError.internalServer('Unexpected error ocurred.');
     return res.sendResponse({ data: product, errors: null });
   };
 
   updateProduct = async (req: Request, res: Response) => {
     const { id } = req.validatedParams;
     const updateProduct = await this.productService.update(id, req.validatedBody);
-    // biome-ignore lint/style/noNonNullAssertion: <not if not found then an error is retrieve>
-    return res.sendResponse({ data: updateProduct!, errors: null });
+    return res.sendResponse({ data: updateProduct, errors: null });
   };
 }
