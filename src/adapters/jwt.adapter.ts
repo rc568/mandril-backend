03a8a63c9c32dedfbcb3d7cd6f845a9ff3f483de
irojas <isaac568r@gmail.com>
2025-09-30 22:13:24 -0500

    add better handling error on jwt.adapter

--- src/adapters/jwt.adapter.ts
+++ src/adapters/jwt.adapter.ts
@@ -2,37 +2,39 @@ import jwt, { type JwtPayload } from 'jsonwebtoken';
 import envs from '../config/envs';
 
 export class Jwt {
-  static generateAccessToken = <T extends Record<string, any>>(payload: T): Promise<string | undefined> => {
-    return new Promise((resolve, rejected) => {
+  static generateAccessToken = <T extends Record<string, any>>(payload: T): Promise<string> => {
+    return new Promise((resolve, reject) => {
       jwt.sign(payload, envs.JWT_SECRET_KEY, { expiresIn: envs.JWT_DURATION }, (error, token) => {
-        if (error) rejected(error);
+        if (error) return reject(error);
+        if (!token) return reject(new Error('Token generation return undefined.'));
         resolve(token);
       });
     });
   };
 
-  static generateRefreshToken = <T extends Record<string, any>>(payload: T): Promise<string | undefined> => {
-    return new Promise((resolve, rejected) => {
+  static generateRefreshToken = <T extends Record<string, any>>(payload: T): Promise<string> => {
+    return new Promise((resolve, reject) => {
       jwt.sign(payload, envs.JWT_REFRESH_SECRET_KEY, { expiresIn: envs.JWT_REFRESH_DURATION }, (error, token) => {
-        if (error) rejected(error);
+        if (error) return reject(error);
+        if (!token) return reject(new Error('Token generation return undefined.'));
         resolve(token);
       });
     });
   };
 
   static verifyAccessToken = <T extends JwtPayload>(token: string): Promise<T | null> => {
-    return new Promise((resolve, rejected) => {
+    return new Promise((resolve) => {
       jwt.verify(token, envs.JWT_SECRET_KEY, (error, decoded) => {
-        if (error) rejected(error);
+        if (error || !decoded || typeof decoded === 'string') return resolve(null);
         resolve(decoded as T);
       });
     });
   };
 
   static verifyRefreshToken = <T extends JwtPayload>(token: string): Promise<T | null> => {
-    return new Promise((resolve, rejected) => {
+    return new Promise((resolve) => {
       jwt.verify(token, envs.JWT_REFRESH_SECRET_KEY, (error, decoded) => {
-        if (error) rejected(error);
+        if (error || !decoded || typeof decoded === 'string') return resolve(null);
         resolve(decoded as T);
       });
     });
