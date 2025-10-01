7b8a04c89d730140e29c71f23127d5db93b16531
irojas <isaac568r@gmail.com>
2025-09-30 20:59:51 -0500

    jwt env added and envs.config update

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,10 +1,23 @@
 import 'dotenv/config';
 import { get } from 'env-var';
+import type { StringValue } from 'ms';
 
-export default {
+const parseJwtDuration = (duration: string): number | StringValue => {
+  if (/^\d+$/.test(duration)) {
+    return Number.parseInt(duration);
+  }
+  return duration as StringValue;
+};
+
+const envs = {
   PORT: get('PORT').required().asPortNumber(),
   PUBLIC_URL: get('PUBLIC_URL').required().asUrlString(),
   DATABASE_URL: get('DATABASE_URL').required().asString(),
   NODE_ENV: get('NODE_ENV').required().asEnum(['production', 'development']),
-  JWT_SECRET_KEY: get('JWT_SECRET_KEY=YourSecretKey').required().asString(),
+  JWT_SECRET_KEY: get('JWT_SECRET_KEY').required().asString(),
+  JWT_REFRESH_SECRET_KEY: get('JWT_REFRESH_SECRET_KEY').required().asString(),
+  JWT_DURATION: parseJwtDuration(get('JWT_DURATION').required().asString()),
+  JWT_REFRESH_DURATION: parseJwtDuration(get('JWT_REFRESH_DURATION').required().asString()),
 };
+
+export default envs;
