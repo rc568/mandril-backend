948e10aa700dc624faa33a8a6cf02edb05ccf2bb
irojas <isaac568r@gmail.com>
2025-10-03 11:28:05 -0500

    remove isActive column from user table

--- src/db/schemas/user.schema.ts
+++ src/db/schemas/user.schema.ts
@@ -14,6 +14,5 @@ export const userTable = pgTable('user', {
   isEmailVerified: boolean().default(false).notNull(),
   role: userRoleEnum().default('employee').notNull(),
   refreshToken: varchar(),
-  isActive: boolean().default(true).notNull(),
   ...softDelete,
 });
