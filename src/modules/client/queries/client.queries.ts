20acabc08d7f973c650834fcd2eb423c6e3fd135
irojas <isaac568r@gmail.com>
2026-07-27 12:54:38 -0500

    update searchClientsQuery to include additional client details

--- src/modules/client/queries/client.queries.ts
+++ src/modules/client/queries/client.queries.ts
@@ -5,11 +5,14 @@ export const searchClientsQuery = (query: string) => {
 
   return sql`
     SELECT DISTINCT ON (c.document_number, c.bussiness_name)
-      c.document_number,
-      c.bussiness_name,
-      c.contact_name,
+      c.id AS "clientId",
+      c.document_type AS "documentType",
+      c.document_number AS "documentNumber",
+      c.bussiness_name AS "bussinessName",
+      c.contact_name AS "contactName",
       c.email,
-      c.phone_number1
+      c.phone_number1 AS "phoneNumber1",
+      c.phone_number2 AS "phoneNumber2"
     FROM
       client c
       INNER JOIN "order" o ON c.id = o.client_id
@@ -17,10 +20,11 @@ export const searchClientsQuery = (query: string) => {
       c.document_number IS NOT NULL
       AND c.bussiness_name IS NOT NULL
       AND (
-            c.bussiness_name ILIKE ${searchTerm}
+        c.bussiness_name ILIKE ${searchTerm}
         OR c.document_number ILIKE ${searchTerm}
         OR c.contact_name ILIKE ${searchTerm}
         OR c.phone_number1 ILIKE ${searchTerm}
+        OR c.phone_number2 ILIKE ${searchTerm}
       )
     ORDER BY
       c.document_number,
