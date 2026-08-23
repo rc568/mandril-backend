0dc2a16f56c2735b168d7440c99b403ac80d4bd6
wsl-acer <isaac568r@gmail.com>
2026-08-23 18:21:46 -0500

    feat(billing): db migration to add amount column on billing_orders

---
+++ drizzle/0009_dark_pixie.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "billing_orders" ADD COLUMN "amount" numeric(12, 6);
--- src/shared/db/schemas/order.schema.ts
+++ src/shared/db/schemas/order.schema.ts
@@ -86,6 +86,7 @@ export const billingOrdersTable = pgTable('billing_orders', {
     .references(() => orderTable.id)
     .notNull(),
   code: varchar({ length: 50 }),
+  amount: decimal({ precision: 12, scale: 6 }),
   status: billingStatusEnum().notNull().default('PENDING'),
   billingReceiptType: receiptTypeEnum().notNull(),
   billingDocumentNumberType: documentNumberTypeEnum().notNull(),
