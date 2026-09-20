4566fd67c72aebc82eb839cfebde4e2398f0c314
wsl-acer <isaac568r@gmail.com>
2026-09-20 07:53:28 -0500

    feat(db): add creation update and soft delete audit helpers

--- src/shared/db/utils/drizzle-columns.ts
+++ src/shared/db/utils/drizzle-columns.ts
@@ -26,3 +26,22 @@ export const userAudit = (userTable: TableWithId) => ({
     .notNull(),
   deletedBy: uuid().references(() => userTable.id),
 });
+
+export const creationAudit = (userTable: TableWithId) => ({
+  createdAt: timestamp({ withTimezone: true }).defaultNow().notNull(),
+  createdBy: uuid()
+    .references(() => userTable.id)
+    .notNull(),
+});
+
+export const updateAudit = (userTable: TableWithId) => ({
+  ...creationAudit(userTable),
+  updatedAt: timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
+  updatedBy: uuid().references(() => userTable.id),
+});
+
+export const softDeleteAudit = (userTable: TableWithId) => ({
+  ...updateAudit(userTable),
+  deletedAt: timestamp({ withTimezone: true }),
+  deletedBy: uuid().references(() => userTable.id),
+});
